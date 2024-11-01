10278a6b6a5cf2b25f1b01d6de142114a47094cc
Rahul <130294117+Rahul-Yadav99@users.noreply.github.com>
2024-11-01 19:10:55 +0530

    Add files via upload

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -32,18 +32,18 @@ const Services = () => {
     ])
   return (
     <Layout>
-        <div className='w-10/12 m-auto py-8'>
+        <div className='md:w-6/12 w-11/12 m-auto py-8'>
             <h1 className='text-3xl font-bold'>Services</h1>
             <div className='h-[1px] bg-[#d1221d] mt-3' />
-            <div className='flex flex-col gap-8 mt-3'>
+            <div className='gap-8 mt-3 grid md:grid-cols-2 grid-cols-1'>
                     {
                         services.map((item) => (
-                            <div key={services.id} className='grid grid-cols-3 bg-white shadow-2xl rounded-lg py-4 px-2'>
-                                <img src={item.image} alt="" className='w-[237px] object-cover col-span-1 ' />
-                                <div className=" flex flex-col justify-center gap-3 col-span-2 pr-4">
+                            <div key={services.id} className=' bg-white shadow-2xl rounded-lg py-4 px-2 flex justify-center flex-col items-center'>
+                                <img src={item.image} alt="" className='w-[337px] object-cover' />
+                                <div className=" flex flex-col justify-center gap-3 mt-3 p-6">
                                     <h1 className='text-3xl font-bold '>{item.title}</h1>
                                     <h1 className='text-xl font-semibold text-gray-600 '>Starting from <span className='font-bold text-2xl text-gray-600'>₹{item.price}</span></h1>
-                                    <p className='text-[14px] text-gray-500'>Base fare is inclusive of 1.0 km distance & 25 minutes of order time. Pricing may vary basis locality. Please note, road tax, parking fee, etc, will be applicable over and above ride fare.</p>
+                                    <p className='text-[14px] text-gray-500'>Base fare is inclusive of 1.0 km distance & 25 minutes of order time. Pricing may vary basis locality.</p>
                                     <Link to={`/booking/${item.id}`} className='block py-2 px-4 bg-[#d1221d] w-fit text-white rounded hover:bg-[#f72822]'>Book Any</Link>
                                 </div>
                             </div>
